07fe9d3cb27ae0e20a9e93805128fb744a8c212f
Mohan Krishna <m.gariki@pccsuk.com>
2021-09-29 10:19:53 +0530

    Revert "Added onEnter Event for testing..."
    This reverts commit a6b2e221

--- lib/flutter_linkify.dart
+++ lib/flutter_linkify.dart
@@ -341,20 +341,14 @@ TextSpan buildTextSpan(
               inlineSpan: TextSpan(
                 text: element.text,
                 style: linkStyle,
-                //recognizer: onOpen != null ? (TapGestureRecognizer()..onTap = () => onOpen(element)) : null,
-                onEnter: (PointerEnterEvent pointerEnterEvent){
-                  print('PointerEnterEvent ${element.text}');
-                }
+                recognizer: onOpen != null ? (TapGestureRecognizer()..onTap = () => onOpen(element)) : null,
               ),
             );
           } else {
             return TextSpan(
               text: element.text,
               style: linkStyle,
-              //recognizer: onOpen != null ? (TapGestureRecognizer()..onTap = () => onOpen(element)) : null,
-              onEnter: (PointerEnterEvent pointerEnterEvent){
-                print('PointerEnterEvent ${element.text}');
-              }
+              recognizer: onOpen != null ? (TapGestureRecognizer()..onTap = () => onOpen(element)) : null,
             );
           }
         } else {
